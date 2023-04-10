a2a1a6387db90f6ce3b94439fbaa6b7faca0c1b4
Rian <121966282+rian256@users.noreply.github.com>
2023-04-11 05:36:10 +0700

    Create PossessTrigger.cs
    // Script to show you are possessed using a canvas and an GameObject as a trigger

---
+++ PossessTrigger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// rian256
+// Script to show you are possessed using a canvas and an GameObject as a trigger
+public class PossessTrigger : MonoBehaviour
+{
+    public GameObject PossessCanvas;
+
+    void OnTriggerEnter(Collider player) {
+        if (player.tag == "Player")
+        {
+            StartCoroutine(GetPossessed());
+            Debug.Log("You just got Possessed");
+        }
+    }
+
+    IEnumerator GetPossessed() 
+    {
+        yield return new WaitForSeconds(3);
+        PossessCanvas.SetActive(true);
+    }
+
+
+    void OnTriggerExit(Collider player)
+    {
+        StartCoroutine(endPossessed());
+    }
+
+    IEnumerator endPossessed()
+    {
+        yield return new WaitForSeconds(5);
+        PossessCanvas.SetActive(false);
+        Destroy(gameObject);
+        Debug.Log("Released");
+    }
+
+}
